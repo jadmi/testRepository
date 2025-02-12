68cb282c4ded8d2e73727d0647dad3b347954dbb
jadmi <152192404+jadmi@users.noreply.github.com>
2025-02-12 18:04:37 +0100

    testen

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello woasasarld!<h1>");
-});
+// homepage route
+app.get("/", handleHome);
+app.get("/about", handleAbout).listen(8000);
+app.get("/welkomst/:username", handleWelkom);
 
-app.listen(8000, () => {
-  console.log("Server is running on port 8000");
-});
+app.use("/static", express.static("static"));
+
+function handleHome(req, res) {
+  res.send("<h1>Hello world!</h1>");
+}
+
+function handleAbout(req, res) {
+  res.send("<h1>About me</h1>");
+}
+
+function handleWelkoM(req, res) {
+  const username = req.params.username;
+  res.send(`<h1>Welkom pagina</h1>, <p>welkom 
+    ${username}, bij mijn node site</p>`);
+}
